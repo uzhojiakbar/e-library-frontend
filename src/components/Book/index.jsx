b1f391c5b059c2015203612f4d76ab8e9ce85567
AzimovAsadbek <azimovasadbek0629@gmail.com>
2024-04-26 03:19:16 +0500

    admin user change table

--- src/components/Book/index.jsx
+++ src/components/Book/index.jsx
@@ -8,14 +8,11 @@ import { ButtonLink } from "../ui/button-link";
 const Book = ({ id, books, setBook }) => {
   const [user] = useState(JSON.parse(localStorage.getItem("user")) || {});
 
-
-
   const [picid, setPicId] = useState(0);
 
   const [BookCurrent, setBookInfo] = useState(false);
   const navigate = useNavigate();
   const [categories, setCategories] = useState([]);
-
 
   const getCategories = async (update = 0) => {
     if (categories.length === 0 || update === 1) {
@@ -34,12 +31,11 @@ const Book = ({ id, books, setBook }) => {
     }
   };
 
-  getCategories()
-
+  getCategories();
 
   const getBook = () => {
     if (!BookCurrent) {
-      books.filter((v) => v.id === id ? setBookInfo(v) : '')
+      books.filter((v) => (v.id === id ? setBookInfo(v) : ""));
     }
   };
 
@@ -49,73 +45,85 @@ const Book = ({ id, books, setBook }) => {
     const BookDoc = doc(db, "files", id);
     await updateDoc(BookDoc, { hidden: false });
     await navigate("/");
-    document.location.reload()
+    document.location.reload();
   };
 
   const getCategory = (id1) => {
-    let a = categories.filter((v) => v.id === id1)
-    return a[0]?.name
-  }
+    let a = categories.filter((v) => v.id === id1);
+    return a[0]?.name;
+  };
 
-  return (
-    BookCurrent ? (
-      <Container>
-        <Images>
-          <div className="thumb">
-            {BookCurrent?.pics.map((v2, i) => (
-              <div key={i} className="carusel-img">
-                <img
-                  onClick={() => setPicId(i)}
-                  src={`https://firebasestorage.googleapis.com/v0/b/ochiqkutubxona-d034a.appspot.com/o/pics%2F${v2.slice(
-                    5
-                  )}?alt=media&token=27b56b0f-821a-45ae-9ccb-f282a53987fd`}
-                  alt="loading..."
-                />
-              </div>
-            ))}
-          </div>
-          <div className="currentPic">
-            <img
-              src={`https://firebasestorage.googleapis.com/v0/b/ochiqkutubxona-d034a.appspot.com/o/pics%2F${BookCurrent.pics[
-                picid
-              ].slice(5)}?alt=media&token=27b56b0f-821a-45ae-9ccb-f282a53987fd`}
-              alt=""
-            />
-          </div>
-        </Images>
-        <BookInformation>
-          <div className="first">
-            <div className="title">{BookCurrent?.name}</div>
-            <div className="text-[20px]"> <span>Muallif:</span>  {BookCurrent?.muallif}</div>
-            <div className="text-[20px]"> <span>chiqgan yili:</span> {BookCurrent?.year}</div>
-            <div className="text-[20px]"> <span>Nashriyot:</span> {BookCurrent?.nashriyot}</div>
-            <div className="text-[20px]"> <span>kategoriya:</span> {getCategory(BookCurrent?.ctg)}</div>
-            <div className="desc">{BookCurrent?.desc}</div>
-            <div className="flex gap-[20px]">
-              <ButtonLink
-                href={`https://firebasestorage.googleapis.com/v0/b/ochiqkutubxona-d034a.appspot.com/o/files%2F${BookCurrent.path.slice(
-                  6
-                )}?alt=media&token=864fc05d-344c-4dd6-81f7-243b5451021b`}
-                className="bg-slate-700 hover:bg-slate-600 button"
-                target="_blank"
-              >
-                Kitobni korish
-              </ButtonLink>
-              {user?.type === "nazoratchi" && BookCurrent?.hidden ? (
-                <ButtonLink onClick={AcceptBook} className="bg-slate-700 hover:bg-slate-600 button"
-                >
-                  Ruhsat berish
-                </ButtonLink>
-              ) : (
-                ""
-              )}
+  return BookCurrent ? (
+    <Container>
+      <Images>
+        <div className="thumb">
+          {BookCurrent?.pics.map((v2, i) => (
+            <div key={i} className="carusel-img">
+              <img
+                onClick={() => setPicId(i)}
+                src={`https://firebasestorage.googleapis.com/v0/b/ochiqkutubxona-d034a.appspot.com/o/pics%2F${v2.slice(
+                  5
+                )}?alt=media&token=27b56b0f-821a-45ae-9ccb-f282a53987fd`}
+                alt="loading..."
+              />
             </div>
+          ))}
+        </div>
+        <div className="currentPic">
+          <img
+            src={`https://firebasestorage.googleapis.com/v0/b/ochiqkutubxona-d034a.appspot.com/o/pics%2F${BookCurrent.pics[
+              picid
+            ].slice(5)}?alt=media&token=27b56b0f-821a-45ae-9ccb-f282a53987fd`}
+            alt=""
+          />
+        </div>
+      </Images>
+      <BookInformation>
+        <div className="first">
+          <div className="title">{BookCurrent?.name}</div>
+          <div className="text-[20px]">
+            {" "}
+            <span>Muallif:</span> {BookCurrent?.muallif}
           </div>
-        </BookInformation>
-      </Container>
-    )
-      :
-      <div>loading</div>
+          <div className="text-[20px]">
+            {" "}
+            <span>chiqgan yili:</span> {BookCurrent?.year}
+          </div>
+          <div className="text-[20px]">
+            {" "}
+            <span>Nashriyot:</span> {BookCurrent?.nashriyot}
+          </div>
+          <div className="text-[20px]">
+            {" "}
+            <span>kategoriya:</span> {getCategory(BookCurrent?.ctg)}
+          </div>
+          <div className="desc">{BookCurrent?.desc}</div>
+          <div className="flex gap-[20px]">
+            <ButtonLink
+              href={`https://firebasestorage.googleapis.com/v0/b/ochiqkutubxona-d034a.appspot.com/o/files%2F${BookCurrent.path.slice(
+                6
+              )}?alt=media&token=864fc05d-344c-4dd6-81f7-243b5451021b`}
+              className="bg-slate-700 hover:bg-slate-600 button"
+              target="_blank"
+            >
+              Kitobni korish
+            </ButtonLink>
+            {user?.type === "nazoratchi" && BookCurrent?.hidden ? (
+              <ButtonLink
+                onClick={AcceptBook}
+                className="bg-slate-700 hover:bg-slate-600 button"
+              >
+                Ruhsat berish
+              </ButtonLink>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </BookInformation>
+    </Container>
+  ) : (
+    <div>loading</div>
   );
 };
 
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,71 +1,83 @@
-import { DataGrid } from '@mui/x-data-grid'
-import React from 'react'
+// import { DataGrid } from "@mui/x-data-grid";
+import React from "react";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 import * as XLSX from "xlsx";
 
-
 const Users = ({ users }) => {
-    console.log(users);
+  const exportXlsx = () => {
+    console.log("export");
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.json_to_sheet(users);
 
-    // "user"
-    // {
-    //     "id": "4yPDrxzJolPO9cPwVNBS",
-    //     "email": "uzhojiakbar3@gmail.com",
-    //     "date": "undefined",
-    //     "type": "user",
-    //     "coins": 100,
-    //     "pass": "undefined",
-    //     "name": "Hojiakbar Murodillayev"
-    // }
+    XLSX.utils.book_append_sheet(wb, ws, "Foydaluvchilar royxati");
 
-    const exportXlsx = () => {
-        console.log("export");
-        const wb = XLSX.utils.book_new();
-        const ws = XLSX.utils.json_to_sheet(users);
+    XLSX.writeFile(wb, "Foydalanuvchilar_ochiq_baza.xlsx");
+  };
 
-        XLSX.utils.book_append_sheet(wb, ws, "Foydaluvchilar royxati");
+  const columns = [
+    { field: "id", headerName: "ID", width: "200" },
+    { field: "name", headerName: "FIO", width: "200" },
+    { field: "email", headerName: "Email", width: "200" },
+    { field: "pass", headerName: "Parol", width: "200" },
+    { field: "type", headerName: "status", width: "200" },
+  ];
+  let edit = false;
 
-        XLSX.writeFile(wb, "Foydalanuvchilar_ochiq_baza.xlsx");
-    };
-
-
-    const columns = [
-        { field: "id", headerName: "ID", width: "200" },
-        { field: "name", headerName: "FIO", width: "200" },
-        { field: "email", headerName: "Email", width: "200" },
-        { field: "pass", headerName: "Parol", width: "200" },
-        { field: "type", headerName: "status", width: "200" },
-    ];
-
-
-
-    return (
-        <div>
-            <div className="flex flex-col gap-[20px]">
-                <div className="flex justify-between">
-                    <h1 className="text-[20px]">Foydaluvchilar: {users.length}</h1>
-                    <h1 className="text-[20px]" onClick={exportXlsx}>
-                        Export
-                    </h1>
-                </div>
-                {/* <DataGrid
-                    rows={users}
-                    columns={columns}
-                    sx={"background-color: #ffffffb4; font-size: 16px;"}
-                    style={{
-                        zIndex: 0
-                    }}
-                    initialState={{
-                        pagination: {
-                            paginationModel: { page: 0, pageSize: 5 },
-                        },
-                    }}
-                    pageSizeOptions={[5, 10, 15]}
-                    checkboxSelection
-                /> */}
-            </div>
+  return (
+    <div>
+      <div className="flex flex-col gap-[20px]">
+        <div className="flex justify-between">
+          <h1 className="text-[20px]">Foydaluvchilar: {users.length}</h1>
+          <h1 className="text-[20px]" onClick={exportXlsx}>
+            Export
+          </h1>
         </div>
-    )
-}
+        <TableContainer
+          className="max-h-[500px] max-w-[1000px] overflow-scroll"
+          component={Paper}
+        >
+          <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                {columns.map((v) => {
+                  return <TableCell key={v.id}>{v.headerName}</TableCell>;
+                })}
+                <TableCell>Edit</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users.map((v, i) => {
+                return (
+                  <TableRow
+                    key={v.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell>{v.id ? v.id : "NULL_RELOAD"}</TableCell>
+                    <TableCell>{v.name}</TableCell>
+                    <TableCell>{v.email}</TableCell>
+                    <TableCell>
+                      {v.pass.length > 9 ? v.pass.slice(26) : "Google orqali"}
+                    </TableCell>
+                    <TableCell>{v.type}</TableCell>
+                    <TableCell>...edit</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
+};
 
-export default Users
+export default Users;
